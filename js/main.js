2ee87673b47713766ddf0551a4ca230612fee618
Tom <13zebras@users.noreply.github.com>
2020-12-31 16:44:39 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 const timer = 20000;
 
 setTimeout(function changeImage() {
+	console.log("changeImage function started");
 	const mainImg = document.getElementById("mainImage");
 	mainImg.classList.replace("one", "zero");
+	console.log("class one changed to zero");
 	const pwidth = document.getElementById("allContent").clientWidth;
 	const pheight = document.getElementById("allContent").clientHeight;
 	const timeStamp = new Date().getTime();
 	const unsplashURL = 'https://loremflickr.com/' + pwidth + '/' + pheight + '/city,night/all?' + timeStamp;
 	mainImg.src = unsplashURL;
+	console.log("URL changed");
 	mainImg.addEventListener("load", function(){
-  		mainImg.classList.replace("zero", "one");
+  		console.log("image loaded, but has it painted yet?");
+		mainImg.classList.replace("zero", "one");
+		console.log("class zero changed to one");
 	});
 	setTimeout(changeImage, timer);
 	
