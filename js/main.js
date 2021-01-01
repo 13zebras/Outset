b0d4fb1c3393ea09561b238c52be9af90b8af2f8
Tom <13zebras@users.noreply.github.com>
2020-12-31 23:53:01 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-const timer = 20000;
+const timer = 12000;
 
+body.addEventListener("load", changeImage)
+
+function changeImage() {
+	console.log("changeImage function started");
+	const mainImg = document.getElementById("mainImage");
+	mainImg.style.opacity = 0;
+	//mainImg.classList.replace("one", "zero");
+	const pwidth = document.getElementById("allContent").clientWidth;
+	const pheight = document.getElementById("allContent").clientHeight;
+	const timeStamp = new Date().getTime();
+	const unsplashURL = 'https://loremflickr.com/' + pwidth + '/' + pheight + '/city,night/all?' + timeStamp;
+	mainImg.src = unsplashURL;
+	mainImg.addEventListener("load", function(){
+		setTimeout(function(){
+			mainImg.style.opacity = 1;
+		}, 1500)
+	});
+	setTimeout(changeImage, timer);
+	
+}
+
+/*
 setTimeout(function changeImage() {
 	console.log("changeImage function started");
 	document.getElementById("imgLd").style.display = "none";
@@ -17,3 +39,4 @@ setTimeout(function changeImage() {
 	setTimeout(changeImage, timer);
 	
 }, timer);
+*/
